4571c16dedb884155af4e6e0cb072a52a8a539ec
Adesz05 <kerekadamtamas@turr.hu>
2022-10-26 10:34:56 +0200

    cc

---
+++ MalomGame/Jatekter.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace MalomGame
+{
+    partial class Jatekter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Jatekter));
+            this.PirosAmong = new System.Windows.Forms.PictureBox();
+            this.PirosFeladas = new System.Windows.Forms.Button();
+            this.KekAmong = new System.Windows.Forms.PictureBox();
+            this.KekFeladas = new System.Windows.Forms.Button();
+            this.PirosGyozelmekSzamaLabel = new System.Windows.Forms.Label();
+            this.PirosNevLabel = new System.Windows.Forms.Label();
+            this.KekGyozelmekSzamaLabel = new System.Windows.Forms.Label();
+            this.KekNevLabel = new System.Windows.Forms.Label();
+            this.Tabla = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PirosAmong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KekAmong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PirosAmong
+            // 
+            this.PirosAmong.Image = ((System.Drawing.Image)(resources.GetObject("PirosAmong.Image")));
+            this.PirosAmong.Location = new System.Drawing.Point(12, 12);
+            this.PirosAmong.Name = "PirosAmong";
+            this.PirosAmong.Size = new System.Drawing.Size(180, 220);
+            this.PirosAmong.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PirosAmong.TabIndex = 9;
+            this.PirosAmong.TabStop = false;
+            // 
+            // PirosFeladas
+            // 
+            this.PirosFeladas.BackColor = System.Drawing.Color.CadetBlue;
+            this.PirosFeladas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PirosFeladas.Font = new System.Drawing.Font("Yu Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PirosFeladas.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PirosFeladas.Location = new System.Drawing.Point(27, 238);
+            this.PirosFeladas.Name = "PirosFeladas";
+            this.PirosFeladas.Size = new System.Drawing.Size(148, 45);
+            this.PirosFeladas.TabIndex = 10;
+            this.PirosFeladas.Text = "Feladás";
+            this.PirosFeladas.UseVisualStyleBackColor = false;
+            // 
+            // KekAmong
+            // 
+            this.KekAmong.Image = ((System.Drawing.Image)(resources.GetObject("KekAmong.Image")));
+            this.KekAmong.Location = new System.Drawing.Point(817, 12);
+            this.KekAmong.Name = "KekAmong";
+            this.KekAmong.Size = new System.Drawing.Size(180, 220);
+            this.KekAmong.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.KekAmong.TabIndex = 11;
+            this.KekAmong.TabStop = false;
+            // 
+            // KekFeladas
+            // 
+            this.KekFeladas.BackColor = System.Drawing.Color.CadetBlue;
+            this.KekFeladas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.KekFeladas.Font = new System.Drawing.Font("Yu Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.KekFeladas.Location = new System.Drawing.Point(834, 238);
+            this.KekFeladas.Name = "KekFeladas";
+            this.KekFeladas.Size = new System.Drawing.Size(148, 45);
+            this.KekFeladas.TabIndex = 12;
+            this.KekFeladas.Text = "Feladás";
+            this.KekFeladas.UseVisualStyleBackColor = false;
+            // 
+            // PirosGyozelmekSzamaLabel
+            // 
+            this.PirosGyozelmekSzamaLabel.AutoSize = true;
+            this.PirosGyozelmekSzamaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PirosGyozelmekSzamaLabel.Location = new System.Drawing.Point(7, 289);
+            this.PirosGyozelmekSzamaLabel.MinimumSize = new System.Drawing.Size(180, 30);
+            this.PirosGyozelmekSzamaLabel.Name = "PirosGyozelmekSzamaLabel";
+            this.PirosGyozelmekSzamaLabel.Size = new System.Drawing.Size(180, 30);
+            this.PirosGyozelmekSzamaLabel.TabIndex = 14;
+            // 
+            // PirosNevLabel
+            // 
+            this.PirosNevLabel.AutoSize = true;
+            this.PirosNevLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PirosNevLabel.Location = new System.Drawing.Point(7, 336);
+            this.PirosNevLabel.MinimumSize = new System.Drawing.Size(180, 30);
+            this.PirosNevLabel.Name = "PirosNevLabel";
+            this.PirosNevLabel.Size = new System.Drawing.Size(180, 30);
+            this.PirosNevLabel.TabIndex = 13;
+            // 
+            // KekGyozelmekSzamaLabel
+            // 
+            this.KekGyozelmekSzamaLabel.AutoSize = true;
+            this.KekGyozelmekSzamaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.KekGyozelmekSzamaLabel.Location = new System.Drawing.Point(817, 289);
+            this.KekGyozelmekSzamaLabel.MinimumSize = new System.Drawing.Size(180, 30);
+            this.KekGyozelmekSzamaLabel.Name = "KekGyozelmekSzamaLabel";
+            this.KekGyozelmekSzamaLabel.Size = new System.Drawing.Size(180, 30);
+            this.KekGyozelmekSzamaLabel.TabIndex = 16;
+            // 
+            // KekNevLabel
+            // 
+            this.KekNevLabel.AutoSize = true;
+            this.KekNevLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.KekNevLabel.Location = new System.Drawing.Point(817, 336);
+            this.KekNevLabel.MinimumSize = new System.Drawing.Size(180, 30);
+            this.KekNevLabel.Name = "KekNevLabel";
+            this.KekNevLabel.Size = new System.Drawing.Size(180, 30);
+            this.KekNevLabel.TabIndex = 15;
+            // 
+            // Tabla
+            // 
+            this.Tabla.Image = ((System.Drawing.Image)(resources.GetObject("Tabla.Image")));
+            this.Tabla.Location = new System.Drawing.Point(198, 12);
+            this.Tabla.Name = "Tabla";
+            this.Tabla.Size = new System.Drawing.Size(615, 605);
+            this.Tabla.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Tabla.TabIndex = 17;
+            this.Tabla.TabStop = false;
+            // 
+            // Jatekter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 616);
+            this.Controls.Add(this.Tabla);
+            this.Controls.Add(this.KekGyozelmekSzamaLabel);
+            this.Controls.Add(this.KekNevLabel);
+            this.Controls.Add(this.PirosGyozelmekSzamaLabel);
+            this.Controls.Add(this.PirosNevLabel);
+            this.Controls.Add(this.KekFeladas);
+            this.Controls.Add(this.KekAmong);
+            this.Controls.Add(this.PirosFeladas);
+            this.Controls.Add(this.PirosAmong);
+            this.MaximumSize = new System.Drawing.Size(1025, 655);
+            this.MinimumSize = new System.Drawing.Size(1025, 655);
+            this.Name = "Jatekter";
+            this.Text = "Jatekter";
+            ((System.ComponentModel.ISupportInitialize)(this.PirosAmong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KekAmong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PirosAmong;
+        private System.Windows.Forms.Button PirosFeladas;
+        private System.Windows.Forms.PictureBox KekAmong;
+        private System.Windows.Forms.Button KekFeladas;
+        private System.Windows.Forms.Label PirosGyozelmekSzamaLabel;
+        private System.Windows.Forms.Label PirosNevLabel;
+        private System.Windows.Forms.Label KekGyozelmekSzamaLabel;
+        private System.Windows.Forms.Label KekNevLabel;
+        private System.Windows.Forms.PictureBox Tabla;
+    }
+}
---
+++ MalomGame/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalomGame
+{
+    class Player
+    {
+        public string Nev,MelyikSzin;
+        public int Pontszam,TablanLevoKorongokSzama, NemTablanLevoKorongokSzama;
+
+        public Player(string nev, string melyikszin, int pontszam, int tablanlevo, int nemtablanlevo)
+        {
+            Nev = nev;
+            MelyikSzin = melyikszin;
+            Pontszam = pontszam;
+            TablanLevoKorongokSzama = tablanlevo;
+            NemTablanLevoKorongokSzama = nemtablanlevo;
+        }
+    }
+}
